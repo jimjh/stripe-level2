54dc91fb4fbd85fa7a756c931843438e9671768b
Jim Lim <jim@quixey.com>
2014-01-23 20:59:04 -0800

    checkpoint

---
+++ client_cache.js
@@ -0,0 +1,4 @@
+// client_cache.js
+// Maintains a small cache of clients and the number of requests they have
+// made. The least recently active clients are evicted.
+// @author Jim Lim - jim@quixey.com
