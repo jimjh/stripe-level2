73393a9b6136187d6cb51b7c0e2b9b0e0239c488
Jim Lim <jim@quixey.com>
2014-03-16 13:56:34 -0700

    replace shield with nginx

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-# Add any build steps you need here
-
 npm install
+
+BASE=$(dirname $0)
+
+# download and build nginx
+if [ ! -d ${BASE}/nginx ]; then
+  [ -f nginx-1.4.6.tar.gz ] || wget http://nginx.org/download/nginx-1.4.6.tar.gz
+  tar -xvf nginx-1.4.6.tar.gz
+  cd nginx-1.4.6
+  ./configure --prefix=$(readlink -f ${BASE}/nginx)
+  make
+  make install
+  cd -
+fi
+
+# ${BASE}/nginx/sbin/nginx -c ${BASE}/conf/nginx.conf
