073846a064557a7572142f0b92f249705046e612
Jim Lim <jim@quixey.com>
2014-03-16 15:44:25 -0700

    nginx with anddos

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 
 npm install
 
-BASE=$(dirname $0)
+BASE=$(readlink -f $(dirname $0))
+
+# download anddos
+[ -d ${BASE}/anddos ] || git clone git@github.com:aufi/anddos.git
 
 # download and build nginx
 if [ ! -d ${BASE}/nginx ]; then
   [ -f nginx-1.4.6.tar.gz ] || wget http://nginx.org/download/nginx-1.4.6.tar.gz
   tar -xvf nginx-1.4.6.tar.gz
   cd nginx-1.4.6
-  ./configure --prefix=$(readlink -f ${BASE}/nginx)
+  ./configure --prefix=$(readlink -f ${BASE}/nginx) --add-module=${BASE}/anddos/anddos
   make
   make install
   cd -
